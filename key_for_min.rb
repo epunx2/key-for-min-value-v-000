6535e76ce396dc50dccfc21a2a5cbf5b41dc1591
Eric Chrobak <eric@ericchrobak.com>
2018-05-07 22:52:38 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value = 0
   counter = 0
-  key_for_min_value = ""
+  key_for_min_value = nil
   name_hash.each do |key, value|
     if counter > 0
       if value < min_value
